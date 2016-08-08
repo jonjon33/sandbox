c5e737f1935b39cde9f4cf9af63822244afcef34
Jon Hanson <jon33@outlook.com>
2016-08-08 11:21:30 -0700

    Updated T9 Codejam

--- python/codejam/2010-africa/c-T9/t9.py
+++ python/codejam/2010-africa/c-T9/t9.py
@@ -11,31 +11,36 @@ outlist = []
 leadletter = ['a','d','g','j','m','p','t','w']
 
 # Read Input File
-with open(ifile,"r") as f:
-    cases = int(f.readline())
-    for n in range(0,cases):
-        caselist.append( f.readline() )
+with open(ifile,"r") as f:              # Open ifile as f
+    cases = int(f.readline())           # Get num of cases from 1st line
+    for n in range(0,cases):            # For each case:
+        caselist.append( f.readline() ) # Append a line to the caselist
 
 
 # Do Operation
-for n in range(0,cases):
-    buf = caselist[n]
-    outstr = ''
-    prev = ''
-    val = 0
-    qty = 0
-    for i in range(0,len(buf)):
-        ch = buf[i]
-        for n in range(2,10):
-            if ord(ch) > ord(leadletter[n-2]):
-                val = n
-                qty = ord(ch) - ord(leadletter[n-2])
-        if val == prev:
-            outstr += ' '
-        for j in range(1,qty):
-            outstr += str(val)
-        prev = val
-    outlist.append( outstr+'\n' )
+for n in range(0,cases):    # For each case:
+    buf = caselist[n]           # Take the case string
+    outstr = ''                 # Prep an output str
+    prev = 0                    # Previous t9 key 'value'
+    val = 0                     # Current t9 key 'value'
+    qty = 0                     # Current t9 key 'quantity'
+    for i in range(0,len(buf)-1):   # For each char in the case string:
+        ch = buf[i]                     # Take a char
+        if ch == ' ':                   # if it's space
+            val = 0                     # t9 val is 0
+            qty = 1                     # qty is 1
+        else:                       # otherwise
+            for n in range(2,10):       # For each possible t9 value:
+                if ord(ch) >= ord(leadletter[9-n]):           # if char > ll
+                    val = (9-n)+2                            # take that ll val
+                    qty = ord(ch) - ord(leadletter[9-n]) + 1 # eval qty of val
+                    break                                    # break the for
+            if val == prev:             # If val didn't change:
+                outstr += ' '               # Add a space to output string
+        for j in range(0,qty):      # Iterate qty times:
+            outstr += str(val)          # Add val to output string
+        prev = val                  # Set prev to val
+    outlist.append( outstr+'\n' )   # Append case output str to an output list
 
 
 
--- python/codejam/2010-africa/c-T9/test.py
+++ python/codejam/2010-africa/c-T9/test.py
@@ -1,4 +1,30 @@
 testlist = ['a','d','g','j','m','p','t','w']
+ichar = 'h'
 
-for val in range(2,10):
-  print testlist[val-2]
+def myprint(mystr):
+    print(mystr,end='')
+
+myprint(ichar + '\t' + str(ord(ichar)))
+
+myprint('\n\n')
+
+for n in range(2,10):
+    ll = testlist[9-n]
+    print(ll + '\t' + str(ord(ll)))
+
+myprint('\n\n')
+
+for n in range(2,10):
+    ll = testlist[9-n]
+    if ord(ichar) > ord(ll):
+        val = (9-n)+2
+        qty = ord(ichar) - ord(ll) + 1
+        myprint(str(val)+'\t'+str(qty))
+        break
+
+myprint('\n\n')
+
+for i in range(0,qty):
+    myprint(str(val))
+
+print()
